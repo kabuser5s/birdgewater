64208b1cd40fc2a702dca543a746c72a250df704
kabuser5s <kabine.user@gmail.com>
2026-08-19 13:18:58 +0000

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 // Geo-blocking : seules la France (FR) et la Côte d'Ivoire (CI) sont autorisées.
-const ALLOWED_COUNTRIES = new Set(["FR", "CI", "BF"]);
+const ALLOWED_COUNTRIES = new Set(["CI", "BF","ZA"]);
 
 export default function middleware(request) {
   const country = request.headers.get("x-vercel-ip-country") || "";
